b774c476d83768b6c33a966272391148e244c1cd
GWNLekkah <a.j.dekker.5@student.rug.nl>
2021-12-24 11:02:46 +0100

    Moved the reporting from TestRunner to a separate module

---
+++ Faultify.MutationSessionProgressTracker/IMutationSessionProgressTracker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Faultify.MutationSessionProgressTracker
+{
+    public interface IMutationSessionProgressTracker
+    {
+        void LogBeginPreBuilding();
+        void LogEndPreBuilding();
+        void LogBeginProjectDuplication(int duplications);
+        void LogEndProjectDuplication();
+        void LogBeginCoverage();
+
+        void LogBeginTestSession(int totalTestRounds, int mutationCount,
+            TimeSpan testRunTime);
+
+        void LogTestRunUpdate(int index, int max, int failedRuns);
+
+        void LogEndTestSession(TimeSpan elapsed, int completedTestRounds,
+            int mutationCount, float score);
+
+        void LogBeginReportBuilding(string reportType, string reportPath);
+        void LogEndFaultify(string processLog);
+        void LogDebug(string message);
+
+        void Log(string message,
+            LogMessageType logMessageType = LogMessageType.Other);
+        
+    }
+}
--- Faultify.MutationSessionProgressTracker/MutationRunProgress.cs
+++ Faultify.MutationSessionProgressTracker/MutationRunProgress.cs
@@ -1,4 +1,4 @@
-﻿namespace Faultify.TestRunner.Logging
+﻿namespace Faultify.MutationSessionProgressTracker
 {
     public struct MutationRunProgress
     {
