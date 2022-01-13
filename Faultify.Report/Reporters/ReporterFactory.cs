14dcd1c74486479eb5af3d9a9e0e31594b6d8334
chrisw889 <chrisjworth1@gmail.com>
2022-01-13 19:55:15 +0100

    Created reporter factory

---
+++ Faultify.Report/Reporters/ReporterFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NLog;
+
+namespace Faultify.Report.Reporters
+{
+    public class ReporterFactory
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static IReporter CreateReporter(string type)
+        {
+            try
+            {
+                return type.ToUpper() switch
+                {
+                    "PDF" => new PdfReporter(),
+                    "HTML" => new HtmlReporter(),
+                    "JSON" => new JsonReporter(),
+                    _ => throw new ArgumentOutOfRangeException(type, $"The argument \"{type}\" is not a valid file output type." +
+                                                                     "Defaulting to JSON."),
+                };
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Logger.Error(ex, ex.Message);
+                return new JsonReporter();
+            }
+        }
+    }
+}
