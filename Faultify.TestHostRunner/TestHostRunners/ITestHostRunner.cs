903e60a4f1239dfa2bf72d1a564ae91af87964ed
GWNLekkah <a.j.dekker.5@student.rug.nl>
2022-01-25 20:55:33 +0100

    Removed unused dependency

--- Faultify.TestHostRunner/TestHostRunners/ITestHostRunner.cs
+++ Faultify.TestHostRunner/TestHostRunners/ITestHostRunner.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Faultify.TestHostRunner.Results;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace Faultify.TestHostRunner.TestHostRunners
